45efef2a27a246170f5c1b94f6ff64ce546b5e77
unknown <luciano.mo@gmail.com>
2022-07-08 12:10:08 -0300

    added redux toolkit example

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import Modal from './components/Modal';
+
+const App = () => {
+  const { isOpen } = useSelector((state) => state.modal);
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+};
+export default App;
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,56 @@
+import { createSlice } from '@reduxjs/toolkit';
+import cartItems from '../../cartItems';
+
+const initialState = {
+  cartItems: cartItems,
+  amount: 0,
+  total: 0,
+  isLoading: true,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    increase: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount += 1;
+    },
+    decrease: (state, action) => {
+      const cartItem = state.cartItems.find(
+        (item) => item.id === action.payload
+      );
+      cartItem.amount -= 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = parseFloat(total.toFixed(2));
+    },
+  },
+});
+
+export const {
+  clearCart,
+  removeItem,
+  increase,
+  decrease,
+  calculateTotals,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
